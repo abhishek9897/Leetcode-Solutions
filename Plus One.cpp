7233208b3b70d51e853e466e2457c16b4c553c4a
Abhishek Chaudhary <76989133+abhishek9897@users.noreply.github.com>
2021-09-11 12:22:20 +0530

    Add files via upload

---
+++ Plus One.cpp
@@ -0,0 +1,20 @@
+ vector<int> plusOne(vector<int>& digits) {
+        
+        int flag=0;
+        for(int i=digits.size()-1;i>=0;i--){
+            if(digits[i]==9){
+                digits[i]=0;
+                
+            }
+            else{
+                
+                    digits[i]=digits[i]+1;
+                    
+                    return digits; 
+            }
+        }
+        digits[0]=1;
+        digits.push_back(0);
+        
+        return digits;
+    }
