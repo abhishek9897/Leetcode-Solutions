0fd81dbc07eee1808a6bdf4762a77018ce8c8d92
Abhishek Chaudhary <76989133+abhishek9897@users.noreply.github.com>
2021-09-11 12:26:26 +0530

    Update Plus One.cpp

--- Plus One.cpp
+++ Plus One.cpp
@@ -1,15 +1,11 @@
- vector<int> plusOne(vector<int>& digits) {
+ vector<int> plusOne(vector<int>& digits) {    
         
-        int flag=0;
         for(int i=digits.size()-1;i>=0;i--){
             if(digits[i]==9){
-                digits[i]=0;
-                
+                digits[i]=0;       
             }
-            else{
-                
-                    digits[i]=digits[i]+1;
-                    
+            else{    
+                    digits[i]=digits[i]+1;                    
                     return digits; 
             }
         }
